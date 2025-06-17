2dc2e6f31e1b5bb1864dc5884e9af331d0115054
Rdjrjqouqcu <rdjrjqouqcu@gmail.com>
2025-06-16 21:19:20 -0700

    Added shield puzzle

--- jame_gam_50/scene/main.gd
+++ jame_gam_50/scene/main.gd
@@ -35,10 +35,12 @@ func redraw_text() -> void:
 	pop_display.text = str(pop, text)
 	chip_display.text = str(collected_chips, "/", total_chips)
 	text = ""
-	var min:int = int(time_elapsed_ms / 60 / 100)
-	var sec:int = int(time_elapsed_ms / 100) % 60
-	var ms:int = int(time_elapsed_ms) % 100
-	time_display.text = "%02d:%02d.%02d" % [min, sec, ms]
+	@warning_ignore("integer_division")
+	var tmin:int = time_elapsed_ms / 60 / 100
+	@warning_ignore("integer_division")
+	var sec:int = floori(time_elapsed_ms / 100) % 60
+	var ms:int = floori(time_elapsed_ms) % 100
+	time_display.text = "%02d:%02d.%02d" % [tmin, sec, ms]
 
 func _init_frames() -> void:
 	for i in range(16):
@@ -52,6 +54,8 @@ func _ready() -> void:
 	_init_frames()
 	redraw_text()
 	add_child(PuzzleCircuit2x1.create_circuit_2x1(Vector2i(2,2)))
+	add_child(PuzzleShield1x1.create_shield_1x1(Vector2i(3,3)))
+	add_child(PuzzleShield1x1.create_shield_1x1(Vector2i(3,4)))
 
 func _process(delta: float) -> void:
 	time_elapsed_ms += delta * 100
---
+++ jame_gam_50/scene/puzzles/shield_1x_1.gd
@@ -0,0 +1,27 @@
+extends PuzzleBase
+class_name PuzzleShield1x1
+
+const SHIELD_1X_1 = preload("res://scene/puzzles/shield_1x1.tscn")
+
+static func create_shield_1x1(location: Vector2i) -> PuzzleShield1x1:
+	var puzzle = SHIELD_1X_1.instantiate()
+	puzzle.position = location * 120
+	return puzzle
+
+func pass_puzzle() -> void:
+	Loggie.info("pass", self.position / 120)
+	queue_free()
+	
+func fail_puzzle() -> void:
+	Loggie.info("fail", self.position / 120)
+	queue_free()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
