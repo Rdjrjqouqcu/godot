016cf477d22ff44761896c54501b273f2e96a3e0
Rdjrjqouqcu <rdjrjqouqcu@gmail.com>
2025-07-07 18:27:20 -0700

    Added ball movement, bouncing and collisions

--- brick_break/block/basic_block.gd
+++ brick_break/block/basic_block.gd
@@ -1,11 +1,18 @@
 extends RigidBody2D
+class_name BasicBlock
 
+@export var variance_degrees: float = 15.0
 
-# Called when the node enters the scene tree for the first time.
+func on_ball_hit() -> void:
+	Loggie.info("ball hit")
+	pass
+
+func get_bounce_degree_offset() -> float:
+	return randf_range(-variance_degrees, variance_degrees)
+
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
