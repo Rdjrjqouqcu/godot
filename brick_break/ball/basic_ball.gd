93880a77d6d0beb7b8036f5f72a43ce92306d222
Rdjrjqouqcu <rdjrjqouqcu@gmail.com>
2025-07-07 18:27:20 -0700

    Added ball movement, bouncing and collisions

--- brick_break/ball/basic_ball.gd
+++ brick_break/ball/basic_ball.gd
@@ -1,25 +1,19 @@
 extends CharacterBody2D
 
+@export var speed: float = 300.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+func _ready() -> void:
+	velocity = Vector2.RIGHT.normalized() * speed
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	velocity = velocity.normalized() * speed
+	var collide = move_and_collide(velocity * delta)
+	if collide != null:
+		var variance_deg = 0
+		if collide.get_collider() is BasicBlock:
+			var block = collide.get_collider() as BasicBlock
+			variance_deg = block.get_bounce_degree_offset()
+			block.on_ball_hit()
+		velocity = velocity.bounce(collide.get_normal()).rotated(deg_to_rad(variance_deg))
