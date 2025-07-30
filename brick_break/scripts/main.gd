42519d77af5033853492117df573e1970a1f8755
eriklamp5 <erikblamp@gmail.com>
2025-07-30 18:27:05 -0400

    basic block spawning

--- brick_break/scripts/main.gd
+++ brick_break/scripts/main.gd
@@ -3,15 +3,22 @@ class_name Main
 
 var block_map: Dictionary[Vector2i, Block] = {}
 
+func has_block_at(pos: Vector2i) -> bool:
+	return block_map.get(pos) != null
+
+func add_block_at(block:Block, pos:Vector2i) -> void:
+	block_map[pos] = block
+	$blocks.add_child(block)
+
 
 func _ready() -> void:
 	# add border
 	for i in range(-1, Globals.BOARD_WIDTH + 1):
-		$border.add_child(Block.new_border_block(Vector2i(-1, i)))
-		$border.add_child(Block.new_border_block(Vector2i(Globals.BOARD_HEIGHT, i)))
+		$border.add_child(Block.new_border_block(Vector2i(-1, i), self))
+		$border.add_child(Block.new_border_block(Vector2i(Globals.BOARD_HEIGHT, i), self))
 	for i in range(0, Globals.BOARD_HEIGHT):
-		$border.add_child(Block.new_border_block(Vector2i(i, -1)))
-		$border.add_child(Block.new_border_block(Vector2i(i, Globals.BOARD_WIDTH)))
+		$border.add_child(Block.new_border_block(Vector2i(i, -1), self))
+		$border.add_child(Block.new_border_block(Vector2i(i, Globals.BOARD_WIDTH), self))
 	# add spawners
 	for i in range(0, Globals.BOARD_WIDTH):
 		for j in range(0, Globals.BOARD_HEIGHT):
