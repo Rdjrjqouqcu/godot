93f4ee28401daf711909ad0dafc6ecd6cae551c0
eriklamp5 <erikblamp@gmail.com>
2025-07-30 18:27:05 -0400

    basic block spawning

--- brick_break/scripts/ball.gd
+++ brick_break/scripts/ball.gd
@@ -6,30 +6,32 @@ const SCALE_DOWN_CURVE = preload("res://sprites/scale_down_curve.tres")
 const CIRCLE_032 = preload("res://sprites/shapes/circle.032.png")
 
 func _play_collision_splash(location: Vector2) -> void:
-	var splash: CPUParticles2D = CPUParticles2D.new()
-	splash.amount = 16
-	splash.texture = CIRCLE_032
-	splash.lifetime = 0.5
-	splash.one_shot = true
-	splash.explosiveness = 1.0
-	splash.gravity = Vector2i.ZERO
-	splash.initial_velocity_min = 150
-	splash.initial_velocity_max = 150
-	splash.spread = 180
-	splash.scale_amount_curve = SCALE_DOWN_CURVE
-	splash.modulate = Color("e84651")
-	splash.global_position = location
-	splash.finished.connect(splash.queue_free)
-	$splashes.add_child(splash)
+	var color_list = [Globals.COLOR_ORANGE, Globals.COLOR_PURPLE]
+	for color in color_list:
+		var splash: CPUParticles2D = CPUParticles2D.new()
+		splash.amount = 16.0 / color_list.size()
+		splash.texture = CIRCLE_032
+		splash.lifetime = 0.5
+		splash.one_shot = true
+		splash.explosiveness = 1.0
+		splash.gravity = Vector2i.ZERO
+		splash.initial_velocity_min = 150
+		splash.initial_velocity_max = 150
+		splash.spread = 180
+		splash.scale_amount_curve = SCALE_DOWN_CURVE
+		splash.modulate = color
+		splash.global_position = location
+		splash.finished.connect(splash.queue_free)
+		$splashes.add_child(splash)
 
 func _on_bounce(col: KinematicCollision2D) -> void:
 	_play_collision_splash(col.get_position())
-	
+
 func _ready() -> void:
+	self.modulate = Globals.COLOR_LIME
 	velocity = Vector2.RIGHT.normalized() * speed
 
 func _physics_process(delta: float) -> void:
-
 	velocity = velocity.normalized() * speed
 	var collide = move_and_collide(velocity * delta)
 	if collide != null:
--- brick_break/scripts/spawner.gd
+++ brick_break/scripts/spawner.gd
@@ -5,15 +5,15 @@ var pos:Vector2i = Vector2i.ZERO
 var root:Main
 
 const SPAWNER = preload("res://scenes/spawner.tscn")
-static func new_spawner(pos: Vector2i, root: Main) -> Spawner:
+static func new_spawner(apos: Vector2i, aroot: Main) -> Spawner:
 	var s = SPAWNER.instantiate()
-	s.pos = pos
-	s.position = Globals.map_position(pos)
-	s.root = root
+	s.pos = apos
+	s.position = Globals.map_position(apos)
+	s.root = aroot
 	return s
 
 func _is_valid_placement() -> bool:
-	return true
+	return not root.has_block_at(pos)
 
 func _on_mouse_on() -> void:
 	Loggie.info("mouse on", pos)
@@ -29,6 +29,7 @@ func _on_mouse_click() -> void:
 	Loggie.info("spawn clicked", pos)
 	if not _is_valid_placement():
 		return
+	root.add_block_at(Block.new_basic_block(pos, root), pos)
 	$Sprite.hide()
 
 func _ready() -> void:
