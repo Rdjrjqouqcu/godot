acc039134bc68482874c3ca51d1490fd3bb533fc
Rdjrjqouqcu <rdjrjqouqcu@gmail.com>
2026-01-06 00:15:22 -0800

    basic random ores

--- mining_sweeper/script/main.gd
+++ mining_sweeper/script/main.gd
@@ -7,6 +7,20 @@ const COLS = 20
 
 var slot_cache: Dictionary[Vector2i, Slot] = {}
 
+func get_ore_map() -> Array[Dictionary]:
+	var red: Dictionary[Vector2i, int] = {}
+	var green: Dictionary[Vector2i, int] = {}
+	var blue: Dictionary[Vector2i, int] = {}
+
+	for i in range(1, 10):
+		red[Vector2i(randi_range(0, COLS - 1), randi_range(0, ROWS - 1))] = i
+	for i in range(1, 10):
+		green[Vector2i(randi_range(0, COLS - 1), randi_range(0, ROWS - 1))] = i
+	for i in range(1, 10):
+		blue[Vector2i(randi_range(0, COLS - 1), randi_range(0, ROWS - 1))] = i
+
+	return [red, green, blue]
+
 func get_neighbor_slots(loc: Vector2i, radius: int = 1) -> Array[Slot]:
 	var res: Array[Slot] = []
 	if radius < 1:
@@ -22,9 +36,17 @@ func get_neighbor_slots(loc: Vector2i, radius: int = 1) -> Array[Slot]:
 func _new_game() -> void:
 	get_tree().call_group(GROUP_SLOTS, "reset")
 
-	slot_cache.get(Vector2i(1,1)).init_ore(1, 0, 0)
-	slot_cache.get(Vector2i(1,2)).init_ore(0, 2, 0)
-	slot_cache.get(Vector2i(1,3)).init_ore(0, 0, 3)
+	var ores: Array[Dictionary] = get_ore_map()
+	for i in ores[0].keys():
+		slot_cache.get(i).add_ore(ores[0].get(i), 0, 0)
+	for i in ores[1].keys():
+		slot_cache.get(i).add_ore(0, ores[1].get(i), 0)
+	for i in ores[2].keys():
+		slot_cache.get(i).add_ore(0, 0, ores[2].get(i))
+
+	slot_cache.get(Vector2i(1,1)).add_ore(1, 0, 0)
+	slot_cache.get(Vector2i(1,2)).add_ore(0, 2, 0)
+	slot_cache.get(Vector2i(1,3)).add_ore(0, 0, 3)
 
 	get_tree().call_group(GROUP_SLOTS, "_update_debug")
 
