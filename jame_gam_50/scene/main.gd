13ec080184f92ede2df7d799f1b4858f4c9f7271
Rdjrjqouqcu <rdjrjqouqcu@gmail.com>
2026-06-09 12:38:13 -0400

    finished circuit 2x1 puzzle, more framework for more puzzles

--- jame_gam_50/scene/main.gd
+++ jame_gam_50/scene/main.gd
@@ -1,22 +1,58 @@
 extends Node2D
+class_name Main
 
-const FRAME = preload("res://scene/frame.tscn")
+enum Difficulty {
+	EASY = 0,
+	MEDIUM = 1,
+	HARD = 2,
+}
+
+@onready var pop_display: RichTextLabel = %popDisplay
+@onready var chip_display: RichTextLabel = %chipDisplay
+@onready var time_display: RichTextLabel = %timeDisplay
+
+const FRAME = preload("res://scene/meta/frame.tscn")
 @onready var frames: Node2D = $frames
+
+var collected_chips = 0
+var total_chips = 10
+var remaining_population = 1_000_000_000
+var time_elapsed_ms = 0
+
+func add_circuit() -> void:
+	collected_chips += 1
+	redraw_text()
+	if collected_chips >= total_chips:
+		Loggie.warn("WIN")
+		# TODO handle WIN
+
+func redraw_text() -> void:
+	var text = ""
+	var pop = remaining_population
+	while pop >= 1000:
+		text += ",%03d" % (pop % 1000)
+		pop /= 1000
+	pop_display.text = str(pop, text)
+	chip_display.text = str(collected_chips, "/", total_chips)
+	text = ""
+	var min:int = int(time_elapsed_ms / 60 / 100)
+	var sec:int = int(time_elapsed_ms / 100) % 60
+	var ms:int = int(time_elapsed_ms) % 100
+	time_display.text = "%02d:%02d.%02d" % [min, sec, ms]
 
 func _init_frames() -> void:
 	for i in range(16):
 		for j in range(9):
-			Loggie.info("adding frame", Vector2i(i,j))
 			var frame = FRAME.instantiate()
 			frame.global_position = Vector2i(i*120, j * 120)
 			frames.add_child(frame)
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	seed(0)
 	_init_frames()
+	redraw_text()
+	add_child(PuzzleCircuit2x1.create_circuit_2x1(Vector2i(2,2)))
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	time_elapsed_ms += delta * 100
+	redraw_text()
