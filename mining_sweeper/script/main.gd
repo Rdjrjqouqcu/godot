b501b2265560584e548f9e6b87d70595d1e38c77
Rdjrjqouqcu <rdjrjqouqcu@gmail.com>
2026-06-09 12:40:16 -0400

    Partial slot implementation

--- mining_sweeper/script/main.gd
+++ mining_sweeper/script/main.gd
@@ -1,9 +1,35 @@
 extends Node2D
+class_name Main
 
+const GROUP_SLOTS = "slots"
+const ROWS = 20
+const COLS = 20
 
-# Called when the node enters the scene tree for the first time.
+var slot_cache: Dictionary[Vector2i, Slot] = {}
+
+func get_neighbor_slots(loc: Vector2i, radius: int = 1) -> Array[Slot]:
+	var res: Array[Slot] = []
+	if radius < 1:
+		return res
+	for x in range(-radius, radius + 1):
+		for y in range(-radius, radius + 1):
+			var nloc = Vector2i(loc.x + x, loc.y + y)
+			if nloc == loc or nloc.x < 0 or nloc.y < 0 or nloc.x >= COLS or nloc.y >= ROWS:
+				continue
+			res.append(slot_cache.get(nloc))
+	return res
+
+func _new_game() -> void:
+	get_tree().call_group(GROUP_SLOTS, "reset")
+
+	slot_cache.get(Vector2i(1,1)).init_ore(1, Slot.ORE_TYPE.IRON)
+	slot_cache.get(Vector2i(1,2)).init_ore(2, Slot.ORE_TYPE.COPPER)
+
+	get_tree().call_group(GROUP_SLOTS, "_update_debug")
+
 func _ready() -> void:
-	pass # Replace with function body.
+	get_tree().call_group.call_deferred(GROUP_SLOTS, "init_main", self)
+	_new_game.call_deferred()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- mining_sweeper/script/slot.gd
+++ mining_sweeper/script/slot.gd
@@ -2,13 +2,60 @@ extends Node2D
 class_name Slot
 
 const size: Vector2i = Vector2i(25, 25)
+enum ORE_TYPE {
+	NONE,
+	IRON,
+	COPPER,
+}
 
 var loc: Vector2i
+var main: Main
+
+var ore_amount: int = 0
+var ore_type: ORE_TYPE = ORE_TYPE.NONE
+var revealed: bool = false
+func has_ore() -> bool:
+	return ore_type != ORE_TYPE.NONE and ore_amount != 0
+
+func init_main(m: Main) -> void:
+	main = m
+	main.slot_cache.set(loc, self)
+
+func init_ore(amt: int, type: ORE_TYPE) -> void:
+	ore_amount = amt
+	ore_type = type
+
+func reset() -> void:
+	ore_amount = 0
+	ore_type = ORE_TYPE.NONE
+	revealed = false
+
+
+func reveal() -> void:
+	if revealed:
+		return
+	revealed = true
+	if has_ore():
+		Loggie.info("found", ore_amount, ore_type)
+	else:
+		var ore_count = {}
+		for n in main.get_neighbor_slots(loc, 1):
+			if n.has_ore():
+				ore_count.set(n.ore_type, n.ore_amount + ore_count.get(n.ore_type, 0))
+		Loggie.info(ore_count)
 
 func _update_debug() -> void:
 	$debug.text = str(
-		loc
+		ore_amount, ore_type
 	)
+
+func _input_event(_view, event, _shape) -> void:
+	if event is InputEventMouseButton:
+		event = event as InputEventMouseButton
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			reveal()
+		#Loggie.info(loc, event)
+
 
 func _ready() -> void:
 	_update_debug()
