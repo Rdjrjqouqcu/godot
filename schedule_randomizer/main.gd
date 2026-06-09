0dec491f2a5c4837fd85578eb835e25b8144321a
Rdjrjqouqcu <rdjrjqouqcu@gmail.com>
2026-06-09 12:42:42 -0400

    scheduleRandomizer added Bored days

--- schedule_randomizer/main.gd
+++ schedule_randomizer/main.gd
@@ -1,181 +1,150 @@
 extends MarginContainer
 
-@onready var date_entry: TextEdit = %dateEntry
-@onready var container: GridContainer = %container
-@onready var err: Label = %err
-@onready var show_roll: CheckButton = %show
+const DATE_FORMAT: String = "%Y-%m-%d"
+const SECONDS_PER_DAY: int = 24 * 60 * 60
 
-const format: String = "%Y-%m-%d"
+const CANCEL: Resource = preload("res://gameicons/cancel.png")
+const CHECK_MARK: Resource = preload("res://gameicons/check-mark.png")
+const STOPWATCH: Resource = preload("res://gameicons/stopwatch.png")
 
-const CANCEL = preload("res://gameicons/cancel.png")
-const CHECK_MARK = preload("res://gameicons/check-mark.png")
-const STOPWATCH = preload("res://gameicons/stopwatch.png")
+@abstract class DataSource:
+	@abstract func _get_data(dt: DateTime, row_offset: int) -> Array[Node]
+	@abstract func get_column_count(show_roll: bool) -> int
+	func _get_roll_node(roll: Variant) -> Node:
+		var r = Label.new()
+		r.text = str("r=", roll, "")
+		r.modulate = Color.GRAY
+		return r
+	func _get_seed_daily(dt: DateTime, offset: int = 0) -> int:
+		return offset * 100000000 + dt.year * 10000 + dt.month * 100 + dt.day
+	func _get_seed_monthly(dt: DateTime, offset: int = 0) -> int:
+		return offset * 100000000 + dt.year * 10000 + dt.month * 100
+	func _random_distributed(seed_used: int, entry_count: int) -> int:
+		var seed_offset = seed_used % entry_count
+		var seed_floored = seed_used - seed_offset
+		var offsets = range(entry_count)
+		seed(seed_floored)
+		offsets.shuffle()
+		return offsets[seed_offset]
+	func add_row(container: GridContainer, dt: DateTime, row_offset: int, show_roll: bool) -> void:
+		for n in _get_data(dt, row_offset).slice(0 if show_roll else 1):
+			container.add_child(n)
 
-enum LineEntryMode {
-	ICON,
-	COUNT,
-	TEXT,
-}
-class LineEntry:
-	var mode: LineEntryMode
-	var line: String
-	var icon: Resource
-	var count: int
-	var color: Color
-	var lset: LabelSettings
-	var weekly:bool = false
-	func add_row(roll: int, container: GridContainer, dt: DateTime, show_roll: bool) -> void:
-		if weekly:
-			if dt.weekday_name != "monday":
-				if show_roll:
-					container.add_child(Label.new())
-				container.add_child(Label.new())
-				container.add_child(Label.new())
-				return
-		if show_roll:
-			var r = Label.new()
-			r.text = str("r=", roll, "")
-			r.modulate = Color.GRAY
-			container.add_child(r)
-		match mode:
-			LineEntryMode.ICON:
-				add_row_icon(container)
-			LineEntryMode.COUNT:
-				add_row_count(container)
-			LineEntryMode.TEXT:
-				add_row_text(container)
+class TimeDataSource extends DataSource:
+	const entries_old = [
+		["Not Today", CANCEL, Color.RED],
+		["After Dinner (7pm)", STOPWATCH, Color.ORANGE],
+		["After Lunch (2pm)", STOPWATCH, Color.YELLOW],
+		["After Lunch (2pm)", STOPWATCH, Color.YELLOW],
+		["Anytime", CHECK_MARK, Color.GREEN],
+		["Anytime", CHECK_MARK, Color.GREEN],
+	]
+	const entries = [
+		["Not Today", CANCEL, Color.RED],  # 4 tasks
+		["Night(7pm)", STOPWATCH, Color.ORANGE], # 3 tasks
+		["Night(7pm)", STOPWATCH, Color.ORANGE], # 3 tasks
+		["Afternoon(3pm)", STOPWATCH, Color.YELLOW], # 2 tasks
+		["Afternoon(3pm)", STOPWATCH, Color.YELLOW], # 2 tasks
+		["Morning(11am)", STOPWATCH, Color.GREEN_YELLOW], # 1 task
+		["Morning(11am)", STOPWATCH, Color.GREEN_YELLOW], # 1 task
+		["Anytime", CHECK_MARK, Color.GREEN], # 0 tasks
+		["Anytime", CHECK_MARK, Color.GREEN], # 0 tasks
+	]
 
-	func set_weekly() -> LineEntry:
-		weekly = true
-		return self
+	func get_column_count(show_roll: bool) -> int:
+		return 2 + (1 if show_roll else 0)
 
-	static func new_icon(l: String, i: Resource, c: Color) -> LineEntry:
-		var n = LineEntry.new()
-		n.mode = LineEntryMode.ICON
-		n.line = l
-		n.icon = i
-		n.color = c
-		return n
-	func add_row_icon(container: GridContainer) -> void:
+	func _get_data(dt: DateTime, row_offset: int) -> Array[Node]:
+		var seed_used = _get_seed_daily(dt, row_offset)
+		var roll = _random_distributed(seed_used, entries.size())
+
+		var line = entries[roll][0]
+		var icon = entries[roll][1]
+		var color = entries[roll][2]
+
 		var icn = TextureRect.new()
 		icn.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 		icn.custom_minimum_size = Vector2(20, 20)
 		icn.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
 		icn.size_flags_vertical = Control.SIZE_SHRINK_CENTER
-		icn.texture = self.icon
-		icn.modulate = self.color
-		container.add_child(icn)
+		icn.texture = icon
+		icn.modulate = color
 
 		var info = Label.new()
-		info.text = self.line
-		container.add_child(info)
-
-	static func new_count(i: int, l: String) -> LineEntry:
-		var n = LineEntry.new()
-		n.mode = LineEntryMode.COUNT
-		n.line = l
-		n.count = i
-		return n
-	func add_row_count(container: GridContainer) -> void:
-		var cnt = Label.new()
-		cnt.text = str(self.count)
-		container.add_child(cnt)
-
-		var info = Label.new()
-		info.text = self.line
-		container.add_child(info)
-
-	static func new_text(l: String, c: Color = Color.WHITE) -> LineEntry:
-		var n = LineEntry.new()
-		n.mode = LineEntryMode.TEXT
-		n.line = l
-		n.color = c
-		return n
-	func add_row_text(container: GridContainer) -> void:
-		var padding = Label.new()
-		padding.text = ""
-		container.add_child(padding)
-		var info = Label.new()
-		info.text = self.line
+		info.text = line
+		info.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 		info.modulate = color
-		container.add_child(info)
 
-var result_error: LineEntry = LineEntry.new_icon("Error", STOPWATCH, Color.PURPLE)
-var results_time: Array[LineEntry] = [
-	LineEntry.new_icon("Not Today", CANCEL, Color.RED),
-	LineEntry.new_icon("After Dinner (7pm)", STOPWATCH, Color.ORANGE),
-	LineEntry.new_icon("After Lunch (2pm)", STOPWATCH, Color.YELLOW),
-	LineEntry.new_icon("After Lunch (2pm)", STOPWATCH, Color.YELLOW),
-	LineEntry.new_icon("Anytime", CHECK_MARK, Color.GREEN),
-	LineEntry.new_icon("Anytime", CHECK_MARK, Color.GREEN),
-]
-var results_tasks: Array[LineEntry] = [
-	LineEntry.new_text("4 tasks", Color.RED),
-	LineEntry.new_text("3 tasks", Color.ORANGE),
-	LineEntry.new_text("3 tasks", Color.ORANGE),
-	LineEntry.new_text("2 tasks", Color.YELLOW),
-	LineEntry.new_text("2 tasks", Color.YELLOW),
-	LineEntry.new_text("1 task", Color.GREEN_YELLOW),
-	LineEntry.new_text("1 task", Color.GREEN_YELLOW),
-	LineEntry.new_text("0 tasks", Color.GREEN),
-	LineEntry.new_text("0 tasks", Color.GREEN),
-]
-var results_restriction: Array[LineEntry] = [
-	LineEntry.new_text("Not Today", Color.RED).set_weekly(),
-	LineEntry.new_text("At Night", Color.YELLOW).set_weekly(),
-	LineEntry.new_text("At Night", Color.YELLOW).set_weekly(),
-	LineEntry.new_text("Anytime", Color.GREEN).set_weekly(),
-	LineEntry.new_text("Anytime", Color.GREEN).set_weekly(),
-	LineEntry.new_text("Anytime", Color.GREEN).set_weekly(),
-]
-func random_raw(s: int, l: int) -> int:
-	return rand_from_seed(s)[0] % l
-func random_distributed(s: int, l: int) -> int:
-	var r = s % l
-	var s2 = s - r
-	var offset = range(l)
-	seed(s2)
-	offset.shuffle()
-	return offset[r]
-var data_randoms: Array[Callable] = [
-	random_distributed,
-	random_distributed,
-]
-var data_sources: Array[Array] = [
-	results_tasks,
-	results_restriction,
+		return [_get_roll_node(roll), info, icn, ]
+
+class RestrictionDataSource extends DataSource:
+	const entries = [
+		["Not Today", Color.RED],
+		["At Night", Color.YELLOW],
+		["At Night", Color.YELLOW],
+		["Anytime", Color.GREEN],
+		["Anytime", Color.GREEN],
+		["Anytime", Color.GREEN],
+	]
+
+	func get_column_count(show_roll: bool) -> int:
+		return 1 + (1 if show_roll else 0)
+
+	func _get_data(dt: DateTime, row_offset: int) -> Array[Node]:
+		var seed_used = _get_seed_daily(dt, row_offset)
+		var roll = _random_distributed(seed_used, entries.size())
+
+		var seed_monthly = _get_seed_monthly(dt, row_offset)
+		seed(seed_monthly)
+		var first_monthly = randi_range(1,14)
+		var second_monthly = randi_range(15,28)
+
+		var roll_set = [roll, first_monthly, second_monthly]
+
+		if dt.day == first_monthly or dt.day == second_monthly:
+			var binfo = Label.new()
+			binfo.text = "BORED"
+			if dt.weekday_name == "monday":
+				binfo.text += entries[roll][0]
+			binfo.modulate = Color.PURPLE
+			return [_get_roll_node(roll_set), binfo]
+
+		if dt.weekday_name != "monday":
+			return [_get_roll_node(roll_set), Label.new()]
+
+		var info = Label.new()
+		info.text = entries[roll][0]
+		info.modulate = entries[roll][1]
+
+		return [_get_roll_node(roll_set), info]
+
+
+var data_sources: Array[DataSource] = [
+	TimeDataSource.new(),
+	RestrictionDataSource.new(),
 ]
 
-func get_or_error(source: Array[LineEntry], index: int) -> LineEntry:
-	if index < 0 or index >= source.size():
-		return result_error
-	return source[index]
-
-func _get_seed(dt: DateTime, offset: int = 0) -> int:
-	return offset * 100000000 + dt.year * 10000 + dt.month * 100 + dt.day
 
 func _add_row(dt: DateTime) -> void:
 	var date = Label.new()
-	date.text = dt.strftime(format + " %a")
-	container.add_child(date)
+	date.text = dt.strftime(DATE_FORMAT + " %a")
+	%container.add_child(date)
 
 	for i in range(len(data_sources)):
-		var source: Array[LineEntry] = data_sources[i]
-		var l: int = source.size()
-		var s: int = _get_seed(dt, i)
-		var val: int = data_randoms[i].call(s, l)
-		var res: LineEntry = get_or_error(source, val)
-		res.add_row(val, container, dt, show_roll.button_pressed)
+		data_sources[i].add_row(%container, dt, i, %show.button_pressed)
 
 func _display_error(txt: String) -> void:
-	err.text = txt
-	err.visible = true
+	%err.text = txt
+	%err.visible = true
 	print("err: ", txt)
 
 func _update() -> void:
-	container.columns = 1 + (2 + (1 if show_roll.button_pressed else 0)) * len(data_sources)
+	var souce_cols = 0
+	for source in data_sources:
+		souce_cols += source.get_column_count(%show.button_pressed)
+	%container.columns = 1 + souce_cols
 
-	var split = date_entry.text.split("-")
+	var split = %dateEntry.text.split("-")
 	if len(split) != 3 or !split[0].is_valid_int() or !split[1].is_valid_int() or !split[2].is_valid_int():
 		_display_error("format error, use YYYY-MM-DD")
 		return
@@ -185,20 +154,19 @@ func _update() -> void:
 		_display_error("parse error, invalid date")
 		return
 
-	err.visible = false
+	%err.visible = false
 
 	print("parsed date: ", dt)
 
-	for child in container.get_children():
+	for child in %container.get_children():
 		child.queue_free()
 
 	for i in range(0, 15):
 		_add_row(dt.add_days(i))
 
-const secondsPerDay = 24 * 60 * 60
 func _ready() -> void:
 	var dt = DateTime.from_timestamp(
-		int(Time.get_unix_time_from_system() - secondsPerDay),
+		int(Time.get_unix_time_from_system() - SECONDS_PER_DAY),
 		Time.get_datetime_dict_from_system()["dst"])
-	date_entry.text = dt.strftime(format)
+	%dateEntry.text = dt.strftime(DATE_FORMAT)
 	_update()
